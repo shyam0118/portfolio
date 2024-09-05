1da929d15f34f28ce4c0b1717f08f32fa755d2c8
Shyamalendu <shyamalendunayak0118@gmail.com>
2024-09-05 23:06:26 +0530

    About text updated

--- components/about.tsx
+++ components/about.tsx
@@ -20,7 +20,7 @@ export default function About() {
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
         After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
+        <span className="font-medium">Computer Science</span>, I decided to pursue my
         passion for programming. I enrolled in a coding bootcamp and learned{" "}
         <span className="font-medium">full-stack web development</span>.{" "}
         <span className="italic">My favorite part of programming</span> is the
