2708d5d8152ba45ceb265cfd866c8c914137a2e0
Shyamalendu <shyamalendunayak0118@gmail.com>
2024-09-23 07:34:20 +0530

    Updated the about section

--- components/about.tsx
+++ components/about.tsx
@@ -21,29 +21,28 @@ export default function About() {
       <p className="mb-3 text-gray-700">
         After graduating with a degree in{" "}
         <span className="font-medium">Computer Science</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
-        <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
+        passion for programming. <span>My favorite part of programming</span> is the
+        problem-solving aspect. I <span className="">love</span> the
         feeling of finally figuring out a solution to a problem. My core stack
         is{" "}
         <span className="font-medium">
           React, Next.js, Node.js, and MongoDB
         </span>
         . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
+        learn new technologies. 
+        {/* I am currently looking for a{" "}
         <span className="font-medium">full-time position</span> as a software
-        developer.
+        developer. */}
       </p>
 
-      <p className="text-gray-700">
+      {/* <p className="text-gray-700">
         <span className="italic">{`When I'm not coding`}</span>, I enjoy playing
         video games, watching movies, and playing with my dog. I also enjoy{" "}
         <span className="font-medium">learning new things</span>. I am currently
         learning about{" "}
         <span className="font-medium">history and philosophy</span>{`. I'm also
         learning how to play the guitar.`}
-      </p>
+      </p> */}
     </motion.section>
   );
 }
