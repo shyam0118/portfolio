1512100b57a6d514135cea2f385214048fdc5068
Shyamalendu <shyamalendunayak0118@gmail.com>
2024-10-26 17:05:46 +0530

    Update

--- lib/data.ts
+++ lib/data.ts
@@ -35,28 +35,28 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "B. Tech",
+    location: "BBSR, Odisha",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+      "Completed my degree in Computer Science and Engineering.",
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    date: "2019 Aug - 2023 Jun",
   },
   {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
+    title: "Software Developer Trainee",
+    location: "BBSR, Odisha",
     description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+      "I worked on several projects using a tech stack that includes React.js, Next.js, Tailwind CSS, Node.js, and GraphQL.",
     icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
+    date: "2023 Jul - 2024 Jan",
   },
   {
     title: "Full-Stack Developer",
-    location: "Houston, TX",
+    location: "BBSR, Odisha",
     description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+      "I'm now working as a full-stack developer. My stack includes React, Next.js, TypeScript, Node.js, Tailwind and MongoDB. I'm open to full-time opportunities.",
     icon: React.createElement(FaReact),
-    date: "2021 - present",
+    date: "2024 Jan - present",
   },
 ] as const;
 
@@ -98,10 +98,7 @@ export const skillsData = [
   "MongoDB",
   "Redux",
   "GraphQL",
-  "Apollo",
   "Express",
   "PostgreSQL",
-  "Python",
-  "Django",
   "Framer Motion",
 ] as const;
