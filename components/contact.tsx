901c728bc4834dee8ff0c2862c6276f79421e733
Shyamalendu <shyamalendunayak0118@gmail.com>
2024-11-17 23:09:40 +0530

    Modified the contact page

--- components/contact.tsx
+++ components/contact.tsx
@@ -41,16 +41,16 @@ export default function Contact() {
 
       <form
         className="mt-10 flex flex-col dark:text-black"
-        action={async (formData) => {
-          const { data, error } = await sendEmail(formData);
+        // action={async (formData) => {
+        //   const { data, error } = await sendEmail(formData);
 
-          if (error) {
-            toast.error(error);
-            return;
-          }
+        //   if (error) {
+        //     toast.error(error);
+        //     return;
+        //   }
 
-          toast.success("Email sent successfully!");
-        }}
+        //   toast.success("Email sent successfully!");
+        // }}
       >
         <input
           className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
