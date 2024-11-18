aca6d7b911971808c73b473ac92a8a3c2de11ff2
Shyamalendu <shyamalendunayak0118@gmail.com>
2024-11-18 07:08:00 +0530

    Added my email

--- components/contact.tsx
+++ components/contact.tsx
@@ -33,7 +33,7 @@ export default function Contact() {
 
       <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
-        <a className="underline" href="mailto:example@gmail.com">
+        <a className="underline" href="mailto:shyamalendunayak0118@gmail.com">
           shyamalendunayak0118@gmail.com
         </a>{" "}
         or through this form.
